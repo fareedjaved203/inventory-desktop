deb047c4f2d3f93ee0f2f582ad16e9563ddfe734
fareedjaved203 <fareedjaved203@gmail.com>
2025-09-21 23:42:25 +0500

    correcting mime type issues in pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
