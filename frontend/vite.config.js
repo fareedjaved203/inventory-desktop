4155a42f98876c90c177ca823e48865d40f1325b
fareedjaved203 <fareedjaved203@gmail.com>
2025-09-22 21:08:26 +0500

    netlify updated 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   publicDir: 'public',
-  base: './',
+  base: '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
