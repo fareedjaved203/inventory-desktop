71a9490b16a6b8031cb419cf64d3c5357ffb93f3
fareedjaved203 <fareedjaved203@gmail.com>
2025-09-22 22:16:12 +0500

    force cache refresh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,9 +13,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined,
-        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
-        chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
-        assetFileNames: `assets/[name]-[hash]-${Date.now()}.[ext]`
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
       }
     }
   },
