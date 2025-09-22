0a87d17aca6bb45e9bd35a4d4eeab549c0f662c5
fareedjaved203 <fareedjaved203@gmail.com>
2025-09-22 22:29:42 +0500

    force cache refresh

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
-  base: '/',
+  base: './', // This is good
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
   },
   server: {
     proxy: {
